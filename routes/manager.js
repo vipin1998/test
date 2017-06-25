2389131b279e8915516ecf00f115bc8108c33e01
vipin1998 <vipindhangar1998@gmail.com>
2017-06-25 19:58:04 +0530

    manager file added

--- routes/manager.js
+++ routes/manager.js
@@ -3,8 +3,10 @@ var managerRoutes = express.Router();
 var User = require('./models/user');
 var Leave = require('./models/leave');
 
+var Verify = require('./verify');
+
 /* GET home page. */
-managerRoutes.put('/approve_application', function(req, res) {
+managerRoutes.put('/approve_application', Verify.checkManager, function(req, res) {
     Leave.find({ '_id' : req.body.id} , function (err , leave)
     {
         leave[0].approvalStatus = "approved"
@@ -19,7 +21,7 @@ managerRoutes.put('/approve_application', function(req, res) {
     });
 });
 
-managerRoutes.get('/get_all_applications', function(req, res) {
+managerRoutes.get('/get_all_applications', Verify.checkManager, function(req, res) {
     Leave.find({} , function (err , leaves)
     {
         res.json(leaves);
--- routes/verify.js
+++ routes/verify.js
@@ -38,3 +38,36 @@ exports.checkAdmin = function (req, res, next)
             }
         }
     };
+
+exports.checkManager = function (req, res, next)
+    {
+        var auth_header = req.headers.authorization;
+        if (null == auth_header)
+        {
+            res.status(401)
+            res.json({
+                "data": {
+                    "error": "user not authenticated"
+                }
+            })
+        }
+        else
+        {
+            var auth = new Buffer(auth_header.split(' ')[1], 'base64').toString().split(':');
+            var user_name = auth[0];
+            var pass = auth[1];
+            if (user_name == 'vipin' && pass == '12345')
+            {
+                next();
+            }
+            else
+            {
+                res.status(401)
+                res.json({
+                    "data": {
+                        "error": "Wrong username or password"
+                    }
+                })
+            }
+        }
+    };
